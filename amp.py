11dfb9e5d7d1fccad052f91ddb394de10abd7dc1
Cuinn Kemp <cuinnkemp@outlook.com>
2023-12-10 14:48:38 +1030

    added funny

--- amp.py
+++ amp.py
@@ -1,32 +1,15 @@
-#%%
+import json
 import librosa
-import matplotlib.pyplot as plt
-import numpy as np
 
-# Load the audio file
-y, sr = librosa.load('test.wav')
-# Only take the first 10 seconds
-ten_seconds = 10 * sr
-y_ten_seconds = y[:ten_seconds]
+# load sound file function
+def load_sound_file(file_path):
+    # Load the audio file using librosa
+    data, sr = librosa.load(str(file_path))
 
-# Calculate the RMS value for each frame
-frame_length = 1024
-hop_length = 512
-rms = librosa.feature.rms(y=y_ten_seconds, frame_length=frame_length, hop_length=hop_length)[0]
-
-# Convert frame indices to time
-frames = range(len(rms))
-t = librosa.frames_to_time(frames, sr=sr, hop_length=hop_length)
+    with open('data.json', 'w') as file:
+        json.dump(data, file)
 
 
-print(t.tolist())
-# Plot
-plt.figure(figsize=(14, 5))
-plt.plot(t, rms, label='Volume over Time')  # Plot the line
-plt.scatter(t, rms, color='red')  # Plot the points
-plt.xlabel('Time (s)')
-plt.ylabel('Volume (RMS Amplitude)')
-plt.title('Volume Over First 10 Seconds')
-plt.legend()
-plt.show()
-# %%
+
+
+
